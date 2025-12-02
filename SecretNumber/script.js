3f48d7a7515e9bb6800cfec1b3c5b65aca29fdcc
AliciaITB <alicia.vazquez@itb.cat>
2025-12-02 20:00:37 +0100

    Comentarios

--- SecretNumber/script.js
+++ SecretNumber/script.js
@@ -1,27 +1,30 @@
+//Declaración de variables
 let intentos;
 let num;
 
+//Obtener un número random
 function piensaRandom() {
-    intentos = 0;
     num = parseInt(Math.random() * 100);
-    console.log("El número es: " + num)
+    console.log("El número es: " + num);
+    intentos = 0;
+    document.getElementById("mensajeUser").innerHTML="";
 }
 
+// Compara si el número del user es mayor, menor o igual.
 function comprueba() {
     let numUser = parseInt(document.getElementById("numUser").value);
-    let msg = ""
-        ; console.log(numUser)
+    let msg = "";
+    //console.log(numUser)
     if (numUser < num) {
         msg = "El número es más grande";
-        intentos++;
     } else {
         if (numUser > num) {
             msg = "El número es más pequeño";
-            intentos++;
         } else {
             msg = "Has acertado el número: " + num + " en " + intentos + " intentos.";
         }
     }
-    console.log(msg);
+    intentos++;
+    //console.log(msg);
     document.getElementById("mensajeUser").innerHTML=msg;
 }
