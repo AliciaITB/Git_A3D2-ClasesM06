853d88c944ab72df609f891f59072e17a2671d26
AliciaITB <alicia.vazquez@itb.cat>
2025-12-10 16:25:31 +0100

    tecladoPrimo

---
+++ DOM/teclado.js
@@ -0,0 +1,50 @@
+
+function crearTeclado() {
+    //Cojo el div del teclado, para luego ir añadiendo
+    let teclado = document.getElementById("teclado");
+
+    for (let i = 1; i < 100; i++) {
+        //Creo la tecla
+        let tecla = document.createElement("div");
+        tecla.innerHTML = "<p>" + i + "</p>";
+        tecla.className = "tecla";
+
+        if ((i % 2) == 0) {
+            //poner fondo azul
+            tecla.style.background = "blue";
+            tecla.style.color = "white";
+        }
+        if ((i % 5) == 0) {
+            //poner fondo azul
+            tecla.style.background = "yellow";
+
+        }
+        if ((i % 3) == 0) {
+            //poner fondo azul
+            tecla.style.background = "red";
+            tecla.style.color = "white";
+        }
+        if (esPrimo(i)) {
+            //poner fondo azul
+            tecla.style.background = "green";
+            tecla.style.color = "white";
+        }
+
+        //Añado la tecla al teclado
+        teclado.appendChild(tecla);
+    }
+}
+
+function esPrimo(num) {
+    if (num < 2) return false;        // 0 y 1 no son primos
+    let limite = Math.sqrt(num);      // optimización
+
+    for (let i = 2; i <= limite; i++) {
+        if (num % i === 0) {
+            return false;             // si es divisible, no es primo
+        }
+    }
+    return true;                       // si no tuvo divisores, es primo
+}
+
+crearTeclado();
